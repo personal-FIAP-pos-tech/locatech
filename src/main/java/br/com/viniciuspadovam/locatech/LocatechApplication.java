96059d44d4f7f55b176be92abbd3f946c7b131f0
viniciuspadovam <padovam3373@gmail.com>
2025-10-27 08:24:02 -0300

    first commit

---
+++ src/main/java/br/com/viniciuspadovam/locatech/LocatechApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciuspadovam.locatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocatechApplication.class, args);
+	}
+
+}
